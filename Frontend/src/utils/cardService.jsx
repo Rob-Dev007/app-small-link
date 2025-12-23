b322706b43225e683d1efbd91ec273baf9060a96
Rob-Dev007 <robdev0695@gmail.com>
2025-12-22 23:23:59 -0500

    style: mejora estilos del componente cardService

--- Frontend/src/utils/cardService.jsx
+++ Frontend/src/utils/cardService.jsx
@@ -5,12 +5,12 @@ const CardService = ({children, title, description})=>{
     const { theme } = UseTheme();
 
     return(
-        <div className={`${theme ==='dark' ? 'bg-black/25' : 'bg-gray-50'} shadow-2xl p-4 rounded-lg max-w-64 flex gap-5 flex-col my-4`}>
+        <div className={`${theme ==='dark' ? 'bg-black/25' : 'bg-gray-50'} flex shadow-2xl p-4 rounded-lg max-w-52 lg:max-w-64 gap-5 flex-col my-4`}>
             <div className="rounded-lg rounded-2xl shadow-xl hover:scale-105 transition bg-gradient-to-br from-cyan-500 via-transparent to-indigo-400 p-4 flex flex-col justify-center items-center gap-3">
                 {children}
-                <h3 className="text-lg font-bold">{title}</h3>
+                <h3 className="text-sm md:text-lg font-bold">{title}</h3>
             </div>
-            <p className=" font-semibold text-lg">{description}</p>
+            <p className="text-sm md:text-lg">{description}</p>
         </div>
     )
 };
