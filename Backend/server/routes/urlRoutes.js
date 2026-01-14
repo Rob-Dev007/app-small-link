89cafc774675dc925f44fca324191bd0651e332b
Rob-Dev007 <robdev0695@gmail.com>
2026-01-14 00:49:45 -0500

    fix(routes): elimina endpoint de incremento manual de clicks

--- Backend/server/routes/urlRoutes.js
+++ Backend/server/routes/urlRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import CheckAuth from '../../middlewares/authMiddleware.js';
-import { agregarUrl, obtenerUrl, actualizarUrl, eliminarUrl, incrementarClicks, pagination } from '../../controllers/urlController.js';
+import { agregarUrl, obtenerUrl, actualizarUrl, eliminarUrl, pagination } from '../../controllers/urlController.js';
 
 const router = express.Router();
 
@@ -14,9 +14,5 @@ router
     .get(CheckAuth, obtenerUrl)
     .put(CheckAuth, actualizarUrl)
     .delete(CheckAuth, eliminarUrl)
-
-router.get('/clicks/:customUrl', CheckAuth, incrementarClicks);
-
-
 
 export default router;
