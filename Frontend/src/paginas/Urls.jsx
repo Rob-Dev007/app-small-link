ff3d3fd12b4eac532dc8b659e129be2d76ed3b02
Rob-Dev007 <robdev0695@gmail.com>
2026-02-12 11:29:13 -0500

    refactor: mejora componente Urls agregando validaciones, estado de copiado y mejoras de UI

--- Frontend/src/paginas/Urls.jsx
+++ Frontend/src/paginas/Urls.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import  { FaTrashAlt, FaCog, FaClipboard } from 'react-icons/fa';
 import  { HiBarsArrowUp } from 'react-icons/hi2';
 import useUrl from '../hooks/useUrl';
@@ -5,14 +6,19 @@ import { confirmDelete, showAlert } from '../helpers/toast';
 
 const Urls = ({ url, mostrarFormEditar })=>{
 
+    if(!url) return null;
+
     const { urlDestino, shortUrlId, customUrl, descripcion, clicks, createdAt } = url;
     const { eliminarUrl } = useUrl();
+
+    const [copied, setCopied] = useState(false);
     
-    const fechaFormateada = new Date(createdAt).toLocaleDateString('es-ES', {
+    const fechaFormateada = createdAt ? 
+    new Date(createdAt).toLocaleDateString('es-ES', {
         year: 'numeric',
         month: 'long',
         day: 'numeric',
-    });
+    }) : 'Fecha no disponible';
 
     const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
@@ -29,8 +35,19 @@ const Urls = ({ url, mostrarFormEditar })=>{
     const handleCopy = ()=>{
         navigator.clipboard
         .writeText(redirectUrl)
-        .then(()=> copyAlert())
-        .catch((error) => console.log('Error al copiar el enlace', error))
+        .then(()=> {
+            setCopied(true)
+            copyAlert()
+            setTimeout(() => setCopied(false), 3000);
+        })
+        .catch((error) => {
+            showAlert({
+            title: '¡Error!',
+            text : 'Url no copiada a portapapeles',
+            icon : 'error',
+        });
+        console.log("error:", error)
+        })
     }
 
     const handleEdit = ()=>{
@@ -55,9 +72,9 @@ const Urls = ({ url, mostrarFormEditar })=>{
     }
 
     return(
-        <div className='p-4 border-2 rounded-lg shadown-lg my-2'>
+        <div className='p-4 rounded-lg my-2 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 shadow-sm hover:shadow-md transition'>
             <div className='flex justify-between flex-col md:flex-row'>
-                <a className='text-blue-400 font-bold max-w-44 lg:max-w-none' href={redirectUrl} target='_blank' >
+                <a className='text-blue-400 font-bold truncate break-all lg:max-w-none' href={redirectUrl} target='_blank' rel="noopener noreferrer">
                     { customUrl || shortUrlId }
                 </a>
                 <div className='flex justify-end md:justify-between items-center gap-1'>
@@ -66,21 +83,25 @@ const Urls = ({ url, mostrarFormEditar })=>{
                         <p className='text-sm lg:text-base'> Clicks: <strong>{ clicks }</strong></p>
                     </div>
                   
-                    <button className="p-2 rounded hover:bg-gray-400 transform translate-y-0 transition duration-500 ease-linear" onClick={ handleCopy }>
+                    <button className={`${copied ? "bg-green-200 text-green-700" : "hover:bg-gray-100"} p-2 rounded hover:scale-105 transition-all duration-200 transform ease-linear`} onClick={ handleCopy } title="Copiar URL">
                         <FaClipboard />
                     </button>
-                    <button className="p-2 rounded hover:bg-gray-400 transform translate-y-0 transition duration-500 ease-linear" onClick={ ()=>handleDelete(url._id) }>
-                        <FaTrashAlt className='text-red-500'/>
+                    <button className="p-2 rounded hover:bg-gray-100 hover:scale-105 transition-all duration-200 transform ease-linear" onClick={ ()=>handleDelete(url._id) }>
+                        <FaTrashAlt className='text-red-500' title='Eliminar url'/>
                     </button>
-                    <button className="p-2 rounded hover:bg-gray-400 transform translate-y-0 transition duration-500 ease-linear" onClick={ handleEdit }>
+                    <button className="p-2 rounded hover:bg-gray-100 hover:scale-105 transition-all duration-200 transform  ease-linear" onClick={ handleEdit } title='Editar url'>
                         <FaCog />
                     </button>
                 </div>
             </div>
-            <div className=''>
+            <div className='flex flex-col'>
                 <p className='my-4 text-gray-500 truncate'> { urlDestino }</p>
-                <div className='flex gap-4 lg:gap-0 flex-col lg:flex-row justify-between'>
-                    <p className='font-semibold break-words'> { descripcion }</p>
+                <div className='flex gap-4 flex-col'>
+                    {descripcion ? (
+                        <p className='font-semibold break-words'>{descripcion}</p>
+                        ) : (
+                        <p className='text-gray-400 italic'><strong>Sin descripción</strong></p>
+                    )}
                     <p> Creado el: <span className='font-light'>{ fechaFormateada }</span></p>
                 </div>
             </div>
